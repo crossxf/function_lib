6c6ad2060ba9a2e0e4e0d1321ac357eb9786d642
Russell <crossxf@gmail.com>
2016-09-15 23:44:07 -0400

    Update r_functions.R
    Added function to copy and paste table

--- R/r_functions.R
+++ R/r_functions.R
@@ -1,4 +1,15 @@
-test <- function()
-{
-  return "Hello world"
+# Copy data out of R
+copy.table <- function(obj, size = 4096) {
+  clip <- paste('clipboard-', size, sep = '')
+  f <- file(description = clip, open = 'w')
+  write.table(obj, f, row.names = FALSE, sep = '\t')
+  close(f)  
+}
+ 
+# Paste data into R
+paste.table <- function() {
+  f <- file(description = 'clipboard', open = 'r')
+  df <- read.table(f, sep = '\t', header = TRUE)
+  close(f)
+  return(df)
 }
